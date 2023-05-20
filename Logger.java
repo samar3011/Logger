0fc1463cb4d8c77ac163754d6d65f7cf23fcdf4e
samar3011 <48644165+samar3011@users.noreply.github.com>
2023-05-20 14:08:49 +0530

    Create Logger.java

---
+++ Logger.java
@@ -0,0 +1,60 @@
+
+import java.io.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+public class Logger extends JFrame implements ActionListener ,Runnable {
+ JButton b1 , b2;
+ JPanel p1;
+ Thread th1;
+ int i = 0;
+ public Logger() {
+	 this.setVisible(true);
+	 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 this.setSize(100,100);
+	 b1 = new JButton("Start");
+	 b2 = new JButton("Stop");
+	 p1 = new JPanel();
+	 p1.add(b1);
+	 p1.add(b2);
+	 this.add(p1);
+	 b1.addActionListener(this);
+	 b2.addActionListener(this);
+	 
+	 
+ }
+ public void actionPerformed(ActionEvent e) {
+	 if(e.getSource() == b1) {
+		 th1 = new Thread(this);
+		 th1.start();
+		 
+	 }
+	 else {
+		 th1.stop();
+		 
+	 }
+ }
+ public void run() {
+	 for(;;) {
+		 try {
+			 Robot r = new Robot();
+			 BufferedImage img = r.createScreenCapture(new Rectangle(getToolkit().getScreenSize()));
+			 ImageIO.write(img, "jpg", new File("d:\\rec\\"+i+".jpg"));
+			 i++;
+			 th1.sleep(300);
+			 
+		 }
+		 catch(Exception e) {
+			 
+		 }
+	 }
+ }
+ public static void main(String args[]) {
+	 new Logger();
+ }
+}
+
